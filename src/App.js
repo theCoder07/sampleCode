e065d3e49ab3fc4b3f848bde9018c438a2873b05
muskansoni482 <muskansoni482@gmail.com>
2024-04-05 17:12:57 +0530

    Thecoder initial code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       TheCoder is Coming soon............
+      </header>
+    </div>
+  );
+}
+
+export default App;
